0d5ca3232c829c40994f1d669b4b195d46eecce9
ridhomauludin <ridhomauludin252@gmail.com>
2024-04-01 12:02:39 +0700

    Initial commit

---
+++ app/src/main/java/com/example/challengedua/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.challengedua
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.service.autofill.Field
+import android.widget.EditText
+
+class MainActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+    }
+}
